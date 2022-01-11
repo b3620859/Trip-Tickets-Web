bba605e0b9c41df6ce7c37a9c5eae99edd62bb3b
Kent@DESKTOP-92HDSMQ <[kent831217@gmail.com]>
2022-01-11 23:29:36 +0800

    0111 添加功能:1.願望清單新增(移除)商品 2.使用cookies 保持登入(目前固定token)

--- Crawler_klook_spring.py
+++ Crawler_klook_spring.py
@@ -1,32 +1,110 @@
 import requests
+from bs4 import BeautifulSoup
 
-url_base ='https://www.klook.com'
-url_api = 'https://www.klook.com/v1/experiencesrv/category/activity?frontend_id_list={}&size={}'  #api frontend_id_list=23==>溫泉,3==主題樂園  size=資料筆數
-# url_hot_springs = 'https://www.klook.com/zh-TW/experiences/cate/23-hot-springs/?frontend_id_list=23&size=24' #溫泉頁面網址
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    'Accept-Language': 'zh-TW',
-    'currency':'TWD',
-    'referer': 'https://www.klook.com/zh-TW/experiences/cate/23-hot-springs/?frontend_id_list=23&size=24',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-}
-#先獲取總共筆數
-url_api_format = url_api.format(23,24)
-res = requests.get(url_api_format,headers = headers)
-ans = res.json()
-total_len = ans['result']['total']
+class Spring_item:
+    def __init__(self):
+        self.id = None
+        self.location = None
+        self.title = None
+        self.rate = None
+        self.market_price = None
+        self.sell_price = None
+        self.link = None
 
-url_api_format = url_api.format(23,total_len)
-res = requests.get(url_api_format,headers = headers)
-result=res.json()['result']['activities']   #現有資料結構
 
-string_dash = "_"
-print(string_dash*100)
-print('| {:5s}| {:10s}| {:10s}| {:40s}| {:30s}|'.format('地點',  '評價', '特價','標題', '連結'))
-for i in result:
-    fix_title = i['title']  #處理標題過長
-    if(len(fix_title)>35):
-        fix_title = fix_title[:35]+'...'
-    print('| {:5s}| {:10s}| {:10s}| {:40s}| {:30s}|'.format( i['location_title'], i['review_star'], i['sell_price']['amount_display'],fix_title, url_base+i['deep_link']))
+class Klook:
 
+    def __init__(self):
+        # https: // www.klook.com / zh - TW / addtocart?is_merge = 1 & shoppingcart_id = 74349624 #購物車
+        self.url_base = 'https://www.klook.com'
+        self.url_login = 'https://log.klook.com/v2/frontlogsrv/log/web'
+        self.url_api = 'https://www.klook.com/v1/experiencesrv/category/activity?frontend_id_list={}&size={}'  # api frontend_id_list=23==>溫泉,3==主題樂園  size=資料筆數
+        # url_hot_springs = 'https://www.klook.com/zh-TW/experiences/cate/23-hot-springs/?frontend_id_list=23&size=24' #溫泉頁面網址
+        self.headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            'Accept-Language': 'zh-TW',
+            'currency': 'TWD',
+            'referer': 'https://www.klook.com/zh-TW/experiences/cate/23-hot-springs/?frontend_id_list=23&size=24',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        }
+
+        #登入用cookie
+        f = open(".\\cookie.txt", 'r')
+        self.cookie3 = f.read()
+        f.close()
+
+    def getItemlist(self, list_id: int):
+        '''
+        get Item List in [list_id] category
+        :param list_id: category id
+        :return: Item List
+        '''
+        # 先獲取總共筆數
+        url_api_format = self.url_api.format(list_id, 24)
+        res = requests.get(url_api_format, headers=self.headers)
+        ans = res.json()
+        total_len = ans['result']['total']
+        url_api_format = self.url_api.format(23, total_len)
+        res = requests.get(url_api_format, headers=self.headers)
+        result = res.json()['result']['activities']  # 現有資料結構
+
+        spring_list: list[Spring_item] = list()
+        for i in result:
+            spring_item = Spring_item()
+            spring_item.id = i['activity_id']
+            spring_item.location = i['location_title']
+            spring_item.title = i['title']
+            spring_item.rate = i['review_star']
+            spring_item.sell_price = i['sell_price']['amount_display']
+            # spring_item.market_price = i['market_price']['amount_display'] #該欄位不一定有值
+            spring_item.link = self.url_base + i['deep_link']
+            spring_list.append(spring_item)
+
+        return spring_list
+
+    def login(self):
+        '''
+        using cookie login
+        :return:
+        '''
+        url ='https://www.klook.com/zh-TW/wishlist'
+        # for i in self.cookie.split("; "):
+        #     cookies = cookies+{i.split("=")[0]:i.split("=")[1]}
+        #     print(cookies)
+        cookies = {i.split("=")[0]:i.split("=")[1] for i in self.cookie3.split("; ")}
+        # print(cookies)
+
+        url_api_format = self.url_api.format(23, 24)
+        res = requests.get(url, headers=self.headers,cookies = cookies)
+        print(res.text)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+
+    def addWishList(self,id:int):
+        url_add = 'https://www.klook.com/v1/usrcsrv/wishlist/add'
+        cookies2 = {i.split("=")[0]:i.split("=")[1] for i in self.cookie3.split("; ")}
+        payload = {"object_id":str(id),"object_type":"act"}
+        res = requests.post(url_add,cookies = cookies2 ,json =payload )
+        result = res.json()
+        print(f'Add wishlist {result["result"]}')
+        return result['success']
+
+    def cancelWishList(self,id:int):
+        url_add = 'https://www.klook.com/v1/usrcsrv/wishlist/cancel'
+        cookies2 = {i.split("=")[0]: i.split("=")[1] for i in self.cookie3.split("; ")}
+        payload = {"object_id": str(id), "object_type": "act"}
+        res = requests.post(url_add, cookies=cookies2, json=payload)
+        result = res.json()
+        print(f'Cancel wishlist {result["result"]}')
+
+        return result['success']
+
+
+
+
+test = Klook()
+test.login()
+test.getItemlist(23)
+test.addWishList(50339)
+test.cancelWishList(50339)
